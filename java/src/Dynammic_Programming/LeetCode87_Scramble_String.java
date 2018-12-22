69e640ab176b50df84902c7ae59e7e257ebe4e66
Earthaa <2892212@qq.com>
2018-12-21 20:45:33 -0800

    fix a typo in 87

--- java/src/Dynammic_Programming/LeetCode87_Scramble_String.java
+++ java/src/Dynammic_Programming/LeetCode87_Scramble_String.java
@@ -7,31 +7,27 @@ There is same argument. if dp[i][j+k-l][l] and dp[i+l][j][k-l] are true, dp[i][j
 */
 public class LeetCode87_Scramble_String {
     public boolean isScramble(String s1, String s2) {
-        if (s1 == null || s2 == null) return false;
-        int m = s1.length();
-        int n = s2.length();
-        if (m != n) return false;
-
-        boolean[][][] dp = new boolean[m][m][m+1];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                dp[i][j][1] = s1.charAt(i) == s2.charAt(j);
+        if(s1.length() != s2.length())
+            return false;
+        int[][][] dp = new int[s1.length()][s1.length()][s1.length() + 1];
+        for(int i = 0; i < s1.length(); i++){
+            for(int j = 0; j < s2.length(); j++){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    dp[i][j][1] = 1;}
             }
         }
-
-        for (int k = 2; k <= m; k++) {
-            for (int i = 0; i <= m - k; i++) {
-                for (int j = 0; j <= m - k; j++) {
-                    dp[i][j][k] = false;
-                    for (int part = 1; part < k; part++) {
-                        if ((dp[i][j][l] && dp[i+l][j+l][k-l])
-                                || (dp[i][j+k-l][l] && dp[i+l][j][k-l])) {
-                            dp[i][j][k] = true;
+        for(int l = 2; l <= s1.length(); l++){
+            for(int i = 0; i <= s1.length() - l; i++){
+                for(int j = 0; j <= s2.length() - l; j++){
+                    for(int k = 1; k < l; k++){
+                        if((dp[i][j][k] == 1 && dp[i+k][j+k][l-k] == 1)||
+                                dp[i][j+l-k][k] == 1 && dp[i+k][j][l-k] == 1){
+                            dp[i][j][l] = 1;
                         }
                     }
                 }
             }
         }
-        return dp[0][0][s1.length()];
+        return dp[0][0][s1.length()] == 1;
     }
 }
